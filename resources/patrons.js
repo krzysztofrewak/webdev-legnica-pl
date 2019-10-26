459c43033a3f34c126564b0cc204e1af2b76057c
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2019-10-26 16:13:24 +0200

    patrons++

--- resources/patrons.js
+++ resources/patrons.js
@@ -1,2 +1,12 @@
 let patrons = [
+	{
+		name: "ITMdevlabs",
+		logo: "/assets/patrons/itmdevlabs.png",
+		url: "https://itmdevlabs.com/",
+	},
+	{
+		name: "Staromiejski Piwowar",
+		logo: "/assets/patrons/staromiejski.png",
+		url: "https://www.facebook.com/Staromiejski-Piwowar-1679133442382062",
+	},
 ]
